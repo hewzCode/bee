cd199810db714b6130838a728828abc8b1b5c621
huz <hewzcode@gmail.com>
2024-03-06 03:10:03 +0100

    com

--- app/wee/page.js
+++ app/wee/page.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Bee = () => {
   return (
-    <div>Bee💜🐝💜</div>
+    <body>
+    <main>
+      <section id='Section 1'>
+        <div>I</div>
+      </section>
+      <section id='Section 2'>
+        <div>love</div>
+      </section>
+      <section id='Section 3'>
+        <div>you</div>
+      </section>
+      <section id='Section 4'>
+        <div>ambee</div>
+      </section>
+      <section id='Section 5'>
+        <div>💜🐝💜</div>
+      </section>
+    </main>
+  </body>
   )
 }
 
